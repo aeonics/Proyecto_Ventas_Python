64290b3cc3b37ce8852e8ca912404c4bb2d85200
Luis R. Alvarez H <aeonics@users.noreply.github.com>
2020-01-14 14:26:02 -0600

    Update main.py
    Colocando Listas para los nombres

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-clients = 'pablo, ricardo, '
+import sys #sys.exit perimte terminar la ejecución del script devolviendo un valor (en Unix/Linux esto se considera una muy buena práctica ya que permite encadenar comandos en función de si terminaron bien, retornando 0 o si tuvieron aglún problema, un valor diferente a 0).
+
+clients =['pablo', 'ricardo']
 
 #Crear CLientes
 def create_client(client_name):
     global clients #variable global y s epuede usar dentro de la funcion
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+        clients.append(client_name)
+        
     else:
         _not_in_list()
 
 #Listar clientes
 def list_clients():
-    global clients
-    print (clients)
+    
+    for idx, client in enumerate(clients):#enumeramos la lista pero con indice
+        print('{}: {}'.format(idx,client))
+
 
 #Actualizar el cliente
-def update_client(client_name, updated_client_name):
+def update_client(client_name, updated_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ', ', updated_client_name + ', ')
+        index = clients.index(client_name)
+        clients[index] = updated_name
     else:
         _not_in_list()
 
@@ -28,32 +33,28 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ', ', '')
+        clients.remove(client_name)
     else:
         _not_in_list()
 
 
 #Encuentra el cliente
 def search_client(client_name):
-    clients_list = clients.split(',')
-
-    for client in clients_list:
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
 
 
-#Añadir una coma
-def _add_comma():
-    global clients
-    clients += ','
+
 
 #Pantalla de menu
 def _print_welcome():
     print('WELCOME TO SALES')
     print('*'*50)
     print('What would like to do? ')
+    print('[L]ist clients')
     print('[C]reate client')
     print('[D]elete client')
     print('[U]pdate client')
@@ -61,9 +62,19 @@ def _print_welcome():
 
 #Obtener el nombre del cliente
 def _get_client_name():
-    return input('What\'s client name: ')
+    client_name = None
+    while not client_name:
+        client_name= input ('What\'s client name: ')
+        if client_name.lower() == 'exit':
+            client_name = None
+            break
 
-#mensajes sin interaccion
+    if not client_name:
+        sys.exit()
+
+    return client_name
+
+#mensajes sin interaccionc
 def _not_in_list():
     print('Client not in list')
 
@@ -92,5 +103,7 @@ if __name__ == "__main__":
             print('The client is in the client\'s list')
         else:
             print('The client : {}  is not in our client\'s list'.format(client_name))
+    elif command.lower() == 'l':
+        list_clients()
     else:
         print('Invalid command!! ')
